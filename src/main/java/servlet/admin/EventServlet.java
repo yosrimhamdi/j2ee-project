a42fac79271e1143c5ebaf1e0dd5933ee3c4d368
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-18 18:24:00 +0100

    POST events/

--- src/main/java/servlet/admin/EventServlet.java
+++ src/main/java/servlet/admin/EventServlet.java
@@ -10,23 +10,41 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.sql.Timestamp;
 
-@WebServlet(name = "EventServlet", value = "/admin/events")
+@WebServlet(name = "EventServlet", value = "/admin/events/new")
 public class EventServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher view = req.getRequestDispatcher("../events/new.jsp");
+        RequestDispatcher view = req.getRequestDispatcher("../../events/new.jsp");
         view.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        String occursAt = request.getParameter("occursAt");
+        String address = request.getParameter("address");
+        String imageUrl = request.getParameter("imageUrl");
+
+        Event event = new Event();
+        event.setTitle(title);
+        event.setDescription(description);
+        event.setAddress(address);
+        event.setImageUrl(imageUrl);
+        event.setOccursAt(Date.valueOf(occursAt));
+
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
 
         try {
             transaction.begin();
+
+            entityManager.persist(event);
 
             transaction.commit();
         } finally {
@@ -37,5 +55,8 @@ public class EventServlet extends HttpServlet {
             entityManager.close();
             entityManagerFactory.close();
         }
+
+        PrintWriter out = response.getWriter();
+        out.println(occursAt);
     }
 }
